268f7f880aa06231361448b002326a0c108a13d0
fanoyrios <aeroporoy10@hotmail.com>
2021-08-08 02:47:58 +0300

    Go to top button

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -15,3 +15,20 @@ dropbtn2.forEach(element => {
         }
       });
   })
+let goToTopButton = document.getElementById('GoToTop');
+
+window.onscroll = function(){
+  if (
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50
+) {
+  goToTopButton.style.display = 'block';
+} else {
+  goToTopButton.style.display = 'none';
+}
+}
+
+goToTopButton.addEventListener('click', function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+})
