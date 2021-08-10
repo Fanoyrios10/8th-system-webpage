437f37584c275c51ce75e144abbb7d170b485076
fanoyrios <aeroporoy10@hotmail.com>
2021-08-10 17:40:53 +0300

    privacy stuff done

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var usefullRouter = require('./routes/usefull');
 var confidentialRouter = require('./routes/confidential');
+var privacyRouter = require('./routes/privacy')
 
 var app = express();
 app.use(session({
@@ -35,6 +36,7 @@ app.use('/', indexRouter);
 app.use('/usefull', usefullRouter);
 app.use('/users', usersRouter);
 app.use('/confidential', confidentialRouter);
+app.use('/privacy', privacyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/privacy.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var dbconnection = require('../lib/db');
+
+/* GET Uniform page. */
+router.get('/terms_of_use', function(req, res, next) {
+  res.render('privacy/terms_of_use');
+});
+
+router.get('/personal_data', function(req, res, next) {
+  res.render('privacy/personal_data');
+});
+
+module.exports = router;
