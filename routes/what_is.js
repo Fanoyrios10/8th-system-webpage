00e979ab0411fa162b73daf3a4c7c49d7be1c8cc
fanoyrios <aeroporoy10@hotmail.com>
2021-08-15 23:24:48 +0300

    done departments and eks

--- routes/what_is.js
+++ routes/what_is.js
@@ -12,4 +12,9 @@ router.get('/departments', function(req, res, next) {
   res.render('what_is/departments');
 });
 
+/* GET eks page. */
+router.get('/eks', function(req, res, next) {
+  res.render('what_is/eks');
+});
+
 module.exports = router;
