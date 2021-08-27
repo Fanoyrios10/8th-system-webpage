d63dfe5093f5440c39e478fd55aef0be715fb625
fanoyrios <aeroporoy10@hotmail.com>
2021-08-27 03:09:30 +0300

    done some work in music

--- routes/scout_group.js
+++ routes/scout_group.js
@@ -6,5 +6,9 @@ router.get('/members', function(req, res, next) {
   res.render('scout_group/members');
 });
 
+/* GET Music page. */
+router.get('/music', function(req, res, next) {
+  res.render('scout_group/music');
+});
 
 module.exports = router;
