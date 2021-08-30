2218f6bb7b57ae599e87dece94c2844d2ffc87c1
fanoyrios <aeroporoy10@hotmail.com>
2021-08-30 15:39:52 +0300

    private scouts page added

--- routes/confidential.js
+++ routes/confidential.js
@@ -34,11 +34,50 @@ router.post('/authorization_cub', function (request, response) {
 });
 
 router.get('/cubs', function(request, response) {
-	if (request.session.loggedin) {
+  if (request.session.loggedin) {
     response.render('confidential/cubs_private');
-	} else {
+  } else {
     response.redirect('confidential/cubs_login');
-	}
-	response.end();
+  }
+  response.end();
 });
+
+router.get('/scouts_login', function (req, res, next) {
+  res.render('confidential/scouts_login', {
+    img: '',
+    alt: ''
+  });
+});
+router.post('/authorization_scout', function (request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username && password) {
+    dbconnection.query('SELECT * FROM confidential WHERE branch = ? AND passphrase = ?', [username, password], function (error, results, fields) {
+      if (results.length > 0) {
+        request.session.loggedin = true;
+        request.session.username = username;
+        response.redirect('/confidential/scouts');
+      } else {
+        response.render('confidential/scouts_login', {
+          img: '/images/confidential/tongue_face.gif',
+          alt: 'φάτσα με γλώσσα'
+        });
+      }
+      response.end();
+    });
+  } else {
+    response.send('Please enter Username and Password!');
+    response.end();
+  }
+});
+
+router.get('/scouts', function(request, response) {
+  if (request.session.loggedin) {
+    response.render('confidential/scouts_private');
+  } else {
+    response.redirect('confidential/scouts_login');
+  }
+  response.end();
+});
+
 module.exports = router;
